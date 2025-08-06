60211a15a88706636123d430ce60773a64865a37
nishant171298@gmail.com <nishant171298@gmail.com>
2025-08-06 10:31:34 +0530

    commiting first few APIs

---
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -0,0 +1,34 @@
+package net.engineeringdigest.journalApp.entity;
+
+public class JournalEntry {
+
+    private long id;
+
+    private String content;
+
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
